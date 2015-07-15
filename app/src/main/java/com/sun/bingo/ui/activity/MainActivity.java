228be83e80181ac710c7795c8530ae008f09cb23
sfsheng0322 <sfsheng0322@gmail.com>
2015-07-15 23:26:15 +0800

    MD风格界面成型

--- app/src/main/java/com/sun/bingo/ui/activity/MainActivity.java
+++ app/src/main/java/com/sun/bingo/ui/activity/MainActivity.java
@@ -2,41 +2,48 @@ package com.sun.bingo.ui.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.sun.bingo.R;
+import com.sun.bingo.adapter.FragmentAdapter;
+import com.sun.bingo.ui.fragment.ListFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
-import cn.bingoogolapple.refreshlayout.BGARefreshLayout;
-import cn.bingoogolapple.refreshlayout.BGARefreshViewHolder;
-import cn.bingoogolapple.refreshlayout.BGAStickinessRefreshViewHolder;
 
 
-public class MainActivity extends BaseActivity implements BGARefreshLayout.BGARefreshLayoutDelegate {
+public class MainActivity extends BaseActivity {
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
-    @InjectView(R.id.text)
-    TextView text;
-    @InjectView(R.id.refresh_layout)
-    BGARefreshLayout refreshLayout;
-    @InjectView(R.id.drawer_view)
-    RelativeLayout drawerView;
-    @InjectView(R.id.drawer)
-    DrawerLayout drawer;
     @InjectView(R.id.tab_layout)
     TabLayout tabLayout;
     @InjectView(R.id.app_bar_layout)
     AppBarLayout appBarLayout;
-    @InjectView(R.id.tv_version_right)
-    LinearLayout tvVersionRight;
+    @InjectView(R.id.view_pager)
+    ViewPager viewPager;
+    @InjectView(R.id.floating_action_button)
+    FloatingActionButton floatingActionButton;
+    @InjectView(R.id.main_coordinator_layout)
+    CoordinatorLayout mainCoordinatorLayout;
+    @InjectView(R.id.main_navigation_layout)
+    NavigationView mainNavigationLayout;
+    @InjectView(R.id.main_drawer_layout)
+    DrawerLayout mainDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,34 +51,48 @@ public class MainActivity extends BaseActivity implements BGARefreshLayout.BGARe
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
+        initView();
+    }
+
+    private void initView() {
         initToolBar(toolbar, false, R.string.app_name);
-        ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, 0, 0);
-        mDrawerToggle.syncState();
-        drawer.setDrawerListener(mDrawerToggle);
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, mainDrawerLayout, toolbar, 0, 0);
+        drawerToggle.syncState();
         initTabLayout();
-        initBGARefreshLayout();
+        initFloatingActionButton();
     }
 
     private void initTabLayout() {
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
-        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        List<String> titles = new ArrayList<>();
+        titles.add("Tab 1");
+        titles.add("Tab 2");
+        tabLayout.addTab(tabLayout.newTab().setText(titles.get(0)));
+        tabLayout.addTab(tabLayout.newTab().setText(titles.get(1)));
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(new ListFragment());
+        fragments.add(new ListFragment());
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabsFromPagerAdapter(adapter);
     }
 
+    private void initFloatingActionButton() {
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Snackbar comes out", Snackbar.LENGTH_LONG)
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(
+                                        MainActivity.this,
+                                        "Toast comes out",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+    }
 
-
-    private void initBGARefreshLayout() {
-        // 为BGARefreshLayout设置代理
-        refreshLayout.setDelegate(this);
-        // 设置下拉刷新和上拉加载更多的风格     参数1：应用程序上下文，参数2：是否具有上拉加载更多功能
-        BGARefreshViewHolder refreshViewHolder = new BGAStickinessRefreshViewHolder(this, true);
-        // 设置下拉刷新和上拉加载更多的风格
-        refreshLayout.setRefreshViewHolder(refreshViewHolder);
-    }
-    @Override
-    public void onBGARefreshLayoutBeginRefreshing(BGARefreshLayout bgaRefreshLayout) {
-    }
-    @Override
-    public boolean onBGARefreshLayoutBeginLoadingMore(BGARefreshLayout bgaRefreshLayout) {
-        return false;
-    }
 }
