7de016de2c9d6a265ba0b689464243df83ef1d17
sfsheng0322 <sfsheng0322@gmail.com>
2015-07-15 23:57:31 +0800

    调整MD风格界面

--- app/src/main/java/com/sun/bingo/ui/activity/DetailActivity.java
+++ app/src/main/java/com/sun/bingo/ui/activity/DetailActivity.java
@@ -3,13 +3,14 @@ package com.sun.bingo.ui.activity;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.sun.bingo.R;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,7 @@ public class DetailActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) this.findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setTitle("我的课程");
@@ -25,5 +27,21 @@ public class DetailActivity extends AppCompatActivity {
 
     public void checkin(View view) {
         Snackbar.make(view, "checkin success!", Snackbar.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
